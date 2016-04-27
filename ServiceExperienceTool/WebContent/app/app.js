1acbbb5fb19f6a3f4037a18b3c5e1d3bf433ff32
Staffan Adolfsson <staffan.ad@gmail.com>
2016-04-27 12:27:37 +0200

    Edited global paths in app module
    Paths are now stored in object app.paths.

--- ServiceExperienceTool/WebContent/app/app.js
+++ ServiceExperienceTool/WebContent/app/app.js
@@ -11,5 +11,8 @@ angular.module('app', [
   $route.reload();
 }])
 .config(function($provide) {
-    $provide.value('APIpath', 'http://78.68.50.137:8080/ServiceExperienceTool/');
+    $provide.value('app.paths', {
+      api: 'http://78.68.50.137:8080/ServiceExperienceTool/',
+      nytimes: 'http://api.nytimes.com/svc/news/v3/content/all/all/.json?api-key=ddbb1c79dda444649d290bc19bd923c6%3A12%3A75027211'
+    });
 });
--- ServiceExperienceTool/WebContent/app/newsfeed/service.js
+++ ServiceExperienceTool/WebContent/app/newsfeed/service.js
@@ -2,9 +2,9 @@
 
 angular.module('newsfeed')
 
-.factory('NewsfetchService', ['$resource',
-  function($resource) {
-    return $resource('http://api.nytimes.com/svc/news/v3/content/all/all/.json?api-key=ddbb1c79dda444649d290bc19bd923c6%3A12%3A75027211', {}, {
+.factory('NewsfetchService', ['$resource', 'app.paths',
+  function($resource, paths) {
+    return $resource(paths.nytimes, {}, {
       query: {
         method: 'GET',
         params: {},
