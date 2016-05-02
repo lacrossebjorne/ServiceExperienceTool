a9c8662906466f1017c32d64a0877d92c5eed0ad
Björn Dalberg <bjorn.dalberg@gmail.com>
2016-05-02 06:48:38 +0200

    authfilter: removed login req

--- ServiceExperienceTool/src/com/set/filters/AuthFilter.java
+++ ServiceExperienceTool/src/com/set/filters/AuthFilter.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-@WebFilter("/app/*")
+@WebFilter("/app/useradmin/*")
 public class AuthFilter implements Filter {
 
 	public void destroy() {
