69d3cf009ff893201ee13c5d64b52a037d38541b
Staffan Adolfsson <staffan.ad@gmail.com>
2016-05-17 10:50:52 +0200

    Created controller for navbar on app-module

---
+++ ServiceExperienceTool/WebContent/static/navbar.controller.js
@@ -0,0 +1,8 @@
+angular.module('app')
+.controller('NavbarController', NavbarController);
+
+NavbarController.$inject = ['$scope', 'app.pushservice'];
+
+function NavbarController($scope, pushservice){
+	
+}
