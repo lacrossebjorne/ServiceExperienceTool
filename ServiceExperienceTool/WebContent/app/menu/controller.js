6f2854bd724b80029b65f6f3505ee8b0a76666ba
rstrandberg <rstrandberg@users.noreply.github.com>
2016-05-19 09:21:05 +0200

    Added filtering on allergens to front-end

--- ServiceExperienceTool/WebContent/app/menu/controller.js
+++ ServiceExperienceTool/WebContent/app/menu/controller.js
@@ -3,11 +3,39 @@
 angular.module('menu')
 
 .controller('MenuController', ['$scope', 'MenuService', 'Category', function($scope, MenuService, Category) {
-
+	
   $scope.menus = MenuService.query();  
   $scope.selectedCat = 0;
   $scope.detailsVisible = false;
-
+  $scope.searchVisible = false;
+  $scope.aDropVisible = false;
+  
+  //testdata
+  $scope.allergenList = [{id: '1', name: 'gluten'},{id: '2', name: 'laktos'},{id: '3', name: 'nötter'},{id: '4', name: 'ägg'}];
+  //end testdata
+  
+  $scope.selectedAllergens = [];
+  
+  $scope.toggleAllergen = function(id) {
+	  var index = $scope.selectedAllergens.indexOf(id);
+	  if(index == -1){
+		  $scope.selectedAllergens.push(id);
+		  this.selected = true;
+	  }else{
+		  $scope.selectedAllergens.splice(index, 1);
+		  this.selected = false
+	  }
+  };
+  
+  $scope.toggleSearchBar = function() {
+	  $scope.searchVisible = !$scope.searchVisible;
+  };
+  
+  $scope.toggleAllergenDropdown = function() {
+	  $scope.aDropVisible = !$scope.aDropVisible;
+	  console.log($scope.aDropVisible);
+  };
+  
   
   $scope.showDetails = function() {
 	  this.detailsVisible = !this.detailsVisible;
@@ -27,6 +55,12 @@ angular.module('menu')
 	  });
 
 	  $scope.menuItems = MenuService.query({menuId: id}, function(result){
+		  //testdata
+		  $scope.menuItems[1].allergens = [{id: '1', name: 'gluten'}];
+		  $scope.menuItems[2].allergens = [{id: '2', name: 'laktos'}];
+		  $scope.menuItems[3].allergens = [{id: '3', name: 'nötter'}];
+		  $scope.menuItems[4].allergens = [{id: '4', name: 'ägg'}];
+		  //end testdata
 		  $scope.itemGroups = groupBy(result, function(item){
 			  return item.category;
 		  });
@@ -36,6 +70,20 @@ angular.module('menu')
   $scope.categoryFilter = function(id){
 	  return function(category){
 		  return (id == 0 || category.id == id || category.parent == id);
+	  }
+  };
+  
+  $scope.allergenFilter = function(inclusive){
+	  return function(item){
+		  if($scope.selectedAllergens.length == 0) return true;
+		  var hit = false;
+		  angular.forEach(item.allergens, function(allergen){
+			  if($scope.selectedAllergens.indexOf(allergen.id) != -1){
+				  hit = true;
+				  return;
+			  }
+		  });
+		  return (inclusive) ? hit : !hit;
 	  }
   };
   
