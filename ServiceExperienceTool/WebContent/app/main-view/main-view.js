82fff5fec0251eb5435606df2168fb55e2a12149
Staffan Adolfsson <staffan.ad@gmail.com>
2016-05-23 16:44:24 +0200

    Hides columns on newsfeed

--- ServiceExperienceTool/WebContent/app/main-view/main-view.js
+++ ServiceExperienceTool/WebContent/app/main-view/main-view.js
@@ -2,35 +2,37 @@
 
 /* App Module */
 
-angular.module('mainView', [
-  'ngRoute',
-  'newsfeed',
-  'newspublish',
-  'menu',
-  'company'
-])
+angular.module('mainView',
+		[ 'ngRoute', 'newsfeed', 'newspublish', 'menu', 'company' ])
 
 /*
- * This routing should only redirect on '.otherwise'.
- * Separate routing in corresponding module.
- *  */
+ * This routing should only redirect on '.otherwise'. Separate routing in
+ * corresponding module.
+ */
 
-.config(['$routeProvider',
-  function($routeProvider) {
-    $routeProvider.otherwise({
-        redirectTo: '/newsfeed'
-      });
-  }
-])
+.config([ '$routeProvider', function($routeProvider) {
+	$routeProvider.otherwise({
+		redirectTo : '/newsfeed'
+	});
+} ])
 
 .controller('MainViewController', MainViewController);
 
-MainViewController.$inject = ['$scope', 'newsfeedservice'];
+MainViewController.$inject = [ '$rootScope', '$scope', '$location', '$log' ];
 
-function MainViewController($scope, newsfeedservice) {
-//	console.log('MainViewController initialized');
-	var vm = this;
-	vm.test = newsfeedservice.test();
-	vm.testRes = newsfeedservice.getMovie('Titanic', '1997', 'short');
+function MainViewController($rootScope, $scope, $location, $log) {
+	$scope.hideCol;
+	hideColumn();
+	$scope.$on("$locationChangeSuccess", function(event, next, current) {
+		$log.info("location changing to:" + next);
+		hideColumn();
+	});
 
+	function hideColumn(url) {
+		$log.info($location.path());
+		if ($location.path() === '/newsfeed')
+			$scope.hideCol = false;
+		else
+			$scope.hideCol = true;
+	}
 };
