09bb17f9eb7847d4b781dbc313a30d03ef185aa9
Mats Heed <heed.mats@gmail.com>
2016-05-24 12:45:40 +0200

    Mats första commit. Har nu lagt in mitt projekt i det hela.

---
+++ ServiceExperienceTool/WebContent/app/Schema/js/controller.js
@@ -0,0 +1,72 @@
+var app = angular.module('myApp', ['ngMaterial'])
+
+    .controller('myCtrl', function ($scope, $mdDialog, $mdMedia) {
+
+        $scope.customFullscreen = $mdMedia('xs') || $mdMedia('sm');
+        $scope.status = ' ';
+        $scope.departmentSelected;
+        $scope.employeeSelected;
+
+        $scope.showAdvanced = function (ev) {
+            var useFullScreen = ($mdMedia('sm') || $mdMedia('xs')) && $scope.customFullscreen;
+            $mdDialog.show({
+                controller: DialogController,
+                templateUrl: 'dialog1.tmpl.html',
+                parent: angular.element(document.body),
+                targetEvent: ev,
+                clickOutsideToClose: true,
+                fullscreen: useFullScreen
+            })
+                .then(function (answer) {
+                    $scope.status = 'You said the information was "' + answer + '".';
+                }, function () {
+                    $scope.status = 'You cancelled the dialog.';
+                });
+            $scope.$watch(function () {
+                return $mdMedia('xs') || $mdMedia('sm');
+            }, function (wantsFullScreen) {
+                $scope.customFullscreen = (wantsFullScreen === true);
+            });
+        };
+
+        function DialogController($scope, $mdDialog) {
+
+            $scope.departments = getDepartments();
+            $scope.employees = getEmployees();
+            
+            $scope.hide = function () {
+                $mdDialog.hide();
+            };
+            $scope.cancel = function () {
+                $mdDialog.cancel();
+            };
+            $scope.answer = function (answer) {
+                $mdDialog.hide(answer);
+            };
+        }
+        function getDepartments() {
+            return [
+                {
+                    id: 1, name: 'Restaurang',
+                    children: [
+                        { id: 1, name: 'Varmkök' },
+                        { id: 2, name: 'Kallis' },
+                        { id: 3, name: 'Servis' }
+                    ]
+                },
+                {
+                    id: 2, name: 'Hotell',
+                    children: [
+
+
+                        { id: 1, name: 'Reception' },
+                        { id: 2, name: 'Städ' },
+                        { id: 3, name: 'Vaktmästariet' }
+                    ]
+                }
+            ];
+        }
+        function getEmployees() {
+            return ['Mats Heed', 'Ingvar Kamprad', 'Anders Filipsson', 'Kalle Eriksson'];
+        }
+    });
