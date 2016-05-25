1d788d3b5119f63dcece59762f13d8b2fb4fa85c
Staffan Adolfsson <staffan.ad@gmail.com>
2016-05-25 16:23:10 +0200

    Added module program-pm

---
+++ ServiceExperienceTool/WebContent/app/program-pm/controller.js
@@ -0,0 +1,68 @@
+'use strict';
+
+angular.module('program-pm')
+
+.controller('OtherProductsController', ['$scope', '$location', 'products.data', 'pushService', function($scope, $location, productsData, pushService) {
+    var imgIndex = 0;
+    var imgUris = productsData.imgUris;
+    var imageSwitch = function() {
+        return productsData.imgUris[imgIndex++ % imgUris.length];
+    }
+
+    $scope.links = productsData.links;
+    $scope.documents = productsData.docs;
+    $scope.tabs = productsData.tabs;
+    $scope.currentTab = $scope.tabs[0].url;
+    $scope.imgA = imageSwitch();
+
+    $scope.onClickTab = function(tab) {
+        $scope.currentTab = tab.url;
+        $scope.imgA = imageSwitch();
+    }
+
+    $scope.isActiveTab = function(tabUrl) {
+        return tabUrl == $scope.currentTab;
+    }
+
+    $scope.addLink = function(title, url) {
+        $scope.links.push({
+            title: title,
+            url: url
+        });
+        pushService.broadcast({
+            path: $location.absUrl(),
+            message: 'Link added: ' + title
+        });
+        $scope.newLinkTitle = "";
+        $scope.newLinkUrl = "";
+    }
+
+    $scope.removeLink = function(index) {
+        $scope.links.splice(index, 1);
+    }
+
+    $scope.addDocument = function(title, url) {
+        $scope.documents.push({
+            title: title,
+            icon: 'fa-file-text-o',
+            url: url
+        });
+        pushService.broadcast({
+            path: $location.absUrl(),
+            message: 'Document added: ' + title
+        });
+        $scope.newDocTitle = "";
+        $scope.upFile = "";
+    }
+
+    $scope.removeDocument = function(index) {
+        $scope.documents.splice(index, 1);
+    }
+
+    $scope.setFile = function(element) {
+        $scope.$apply(function($scope) {
+            $scope.upFile = element.files[0].name;
+            console.log(element.files[0].name);
+        });
+    };
+}]);
---
+++ ServiceExperienceTool/WebContent/app/program-pm/other-products.js
@@ -0,0 +1,38 @@
+'use strict';
+
+angular.module('program-pm', [
+  'ngRoute',
+  'ngResource'
+])
+
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/other-products', {
+    templateUrl: 'app/other-products/other-products.html',
+    controller: 'OtherProductsController'
+  });
+}])
+
+.config(function($provide) {
+  $provide.value('products.data', {
+    tabs: [{
+      title: 'Logi',
+      url: 'app/other-products/templates/tabs/tab.logi.html'
+    }, {
+      title: 'Paket',
+      url: 'app/other-products/templates/tabs/tab.paket.html'
+    }, {
+      title: 'Aktiviteter',
+      url: 'app/other-products/templates/tabs/tab.aktiviteter.html'
+    }, {
+      title: 'Special',
+      url: 'app/other-products/templates/tabs/tab.special.html'
+    }],
+
+    imgUris: [
+      'img/set_mock-img_01.jpg',
+      'img/set_mock-img_02.jpg',
+      'img/set_mock-img_03.jpg',
+      'img/set_mock-img_04.jpg'
+    ]
+  });
+});
