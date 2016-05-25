92d4b180db949c39ba5a10f76c0dadd7f443ef69
Staffan Adolfsson <staffan.ad@gmail.com>
2016-05-25 11:59:57 +0200

    Minor fix

--- ServiceExperienceTool/WebContent/app/company/controller.js
+++ ServiceExperienceTool/WebContent/app/company/controller.js
@@ -3,53 +3,66 @@
 angular.module('company')
 
 .controller('CompanyController', ['$scope', '$location', 'company.data', 'pushService', function($scope, $location, companyData, pushService) {
-  var imgIndex = 0; 
-  var imgUris = companyData.imgUris;
-  var imageSwitch = function() {
-    return imgUris[imgIndex++ % imgUris.length];
-  }
+    var imgIndex = 0;
+    var imgUris = companyData.imgUris;
+    var imageSwitch = function() {
+        return imgUris[imgIndex++ % imgUris.length];
+    }
 
-  $scope.links = companyData.links;
-  $scope.documents = companyData.docs;
-  $scope.tabs = companyData.tabs;
-  $scope.currentTab = 'app/company/templates/tabs/tab.vision-affarside.html';
-  $scope.imgA = imageSwitch();
-
-  $scope.onClickTab = function(tab) {
-    $scope.currentTab = tab.url;
+    $scope.links = companyData.links;
+    $scope.documents = companyData.docs;
+    $scope.tabs = companyData.tabs;
+    $scope.currentTab = 'app/company/templates/tabs/tab.vision-affarside.html';
     $scope.imgA = imageSwitch();
-  }
 
-  $scope.isActiveTab = function(tabUrl) {
-    return tabUrl == $scope.currentTab;
-  }
+    $scope.onClickTab = function(tab) {
+        $scope.currentTab = tab.url;
+        $scope.imgA = imageSwitch();
+    }
 
-  $scope.addLink = function(title, url){
-      $scope.links.push({title: title, url: url});
-      pushService.broadcast({path: $location.absUrl(), message: 'Link added: ' + title});
-      $scope.newLinkTitle = "";
-      $scope.newLinkUrl = "";
-  }
+    $scope.isActiveTab = function(tabUrl) {
+        return tabUrl == $scope.currentTab;
+    }
 
-  $scope.removeLink = function(index){
-      $scope.links.splice(index, 1);
-  }
+    $scope.addLink = function(title, url) {
+        $scope.links.push({
+            title: title,
+            url: url
+        });
+        pushService.broadcast({
+            path: $location.absUrl(),
+            message: 'Link added: ' + title
+        });
+        $scope.newLinkTitle = "";
+        $scope.newLinkUrl = "";
+    }
 
-  $scope.addDocument = function(title, url){
-      $scope.documents.push({title: title, icon:'fa-file-text-o', url: url});
-      pushService.broadcast({path: $location.absUrl(), message: 'Document added: ' + title});
-      $scope.newDocTitle = "";
-      $scope.upFile = "";
-  }
+    $scope.removeLink = function(index) {
+        $scope.links.splice(index, 1);
+    }
 
-  $scope.removeDocument = function(index){
-      $scope.documents.splice(index, 1);
-  }
+    $scope.addDocument = function(title, url) {
+        $scope.documents.push({
+            title: title,
+            icon: 'fa-file-text-o',
+            url: url
+        });
+        pushService.broadcast({
+            path: $location.absUrl(),
+            message: 'Document added: ' + title
+        });
+        $scope.newDocTitle = "";
+        $scope.upFile = "";
+    }
 
-  $scope.setFile = function(element) {
-    $scope.$apply(function($scope) {
-        $scope.upFile = element.files[0].name;
-        console.log(element.files[0].name);
-    });
-};
+    $scope.removeDocument = function(index) {
+        $scope.documents.splice(index, 1);
+    }
+
+    $scope.setFile = function(element) {
+        $scope.$apply(function($scope) {
+            $scope.upFile = element.files[0].name;
+            console.log(element.files[0].name);
+        });
+    };
 }]);
