2d5c5bc93e871abc1e0835b3fb2329d73cb2ef53
Staffan Adolfsson <staffan.ad@gmail.com>
2016-05-25 15:17:03 +0200

    Fixed error with images

--- ServiceExperienceTool/WebContent/app/company/controller.js
+++ ServiceExperienceTool/WebContent/app/company/controller.js
@@ -4,6 +4,7 @@ angular.module('company')
 
 .controller('CompanyController', ['$scope', '$location', 'company.data', 'pushService', function($scope, $location, companyData, pushService) {
     var imgIndex = 0;
+    var imgUris = companyData.imgUris;
     var imageSwitch = function() {
         return companyData.imgUris[imgIndex++ % imgUris.length];
     }
