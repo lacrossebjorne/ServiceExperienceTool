22136ebce20c9a8ae1f91310cf6ec6f948ad28e7
Staffan Adolfsson <staffan.ad@gmail.com>
2016-05-30 16:44:30 +0200

    Documentation for pushService

--- ServiceExperienceTool/WebContent/app/app.pushservice.js
+++ ServiceExperienceTool/WebContent/app/app.pushservice.js
@@ -3,10 +3,27 @@ angular.module('app')
 
 pushService.$inject = ['$rootScope'];
 
+/**
+ * Emits events to be used as pushservice from other modules.
+ * 
+ * @param $rootScope
+ */
 function pushService($rootScope) {
+	
+	/**
+	 * Emits event, making the event exclusive for rootscope.
+	 * 
+	 * @param	message	text to display
+	 */
 	this.broadcast = function(message) {
 		$rootScope.$emit("pushEvent", message);
 	}
+	
+	/**
+	 * Listens for emitted events.
+	 * 
+	 * @param	callback	function to perform as callback
+	 */
 	this.listen = function(callback) {
 		$rootScope.$on("pushEvent", callback);
 	}
